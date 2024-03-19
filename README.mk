1163195b1b05600740ed87f212dee7d46d5ad618
Eder TS <115313647+Eder-TS@users.noreply.github.com>
2024-03-19 20:56:40 -0300

    Update README.mk

--- README.mk
+++ README.mk
@@ -1,1 +1,4 @@
-Excercise to pratice the use of Bootstrap.
+Excercise to pratice the use of Bootstrap framework.<br>
+This is my first time using this framework.<br>
+I thought it was cool whit repetitive element, but a little overwhelming with customizations.
+You can check out here: https://dnc-bank.netlify.app/
